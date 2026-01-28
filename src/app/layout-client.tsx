4fe6c345df0d7712510e698d6d52adf8f88a97d9
Ayopee <Ayopee001@gmail.com>
2026-01-28 15:17:28 +0700

    update V1

--- src/app/layout-client.tsx
+++ src/app/layout-client.tsx
@@ -1,7 +1,7 @@
 // app/RootLayoutClient.tsx
 "use client";
 
-import { useState } from "react";
+import { useState, Suspense } from "react";
 import Navbar from "@/app/components/Navbar";
 import DrawerMenu from "@/app/components/DrawerMenu";
 import QueryString from "@/app/components/QueryString";
@@ -12,7 +12,10 @@ export default function RootLayoutClient({ children }: { children: React.ReactNo
 
     return (
         <AuthProvider>
-            <QueryString />
+            <Suspense fallback={null}>
+                <QueryString />
+            </Suspense>
+
             <Navbar onOpenMenu={() => setOpen(true)} />
             <DrawerMenu open={open} onClose={() => setOpen(false)} />
             {children}
