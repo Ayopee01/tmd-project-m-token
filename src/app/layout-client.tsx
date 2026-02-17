f3e6032a4320472e1a79a1901ffa3812cbd46c01
Ayopee <ayopee001@gmail.com>
2026-02-17 15:08:12 +0700

    SwipeBack
    SwipeBack

--- src/app/layout-client.tsx
+++ src/app/layout-client.tsx
@@ -7,6 +7,7 @@ import DrawerMenu from "./components/DrawerMenu_Sticky";
 import Footer from "./components/Footer";
 import QueryString from "@/app/components/QueryString";
 import { AuthProvider } from "@/app/hooks/auth-hook";
+import SwipeBack from "@/app/components/SwipeBack";
 
 export default function RootLayoutClient({ children }: { children: React.ReactNode }) {
     const [open, setOpen] = useState(false);
@@ -16,9 +17,15 @@ export default function RootLayoutClient({ children }: { children: React.ReactNo
             <Suspense fallback={null}>
                 <QueryString />
             </Suspense>
+
             <Navbar onOpenMenu={() => setOpen((i) => !i)} />
             <DrawerMenu open={open} onClose={() => setOpen(false)} />
-            {children}
+
+            {/* ✅ ปัดย้อนเฉพาะหน้า content */}
+            <SwipeBack>
+                {children}
+            </SwipeBack>
+
             <Footer />
         </AuthProvider>
     );
