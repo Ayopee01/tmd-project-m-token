91fcf7a8b971c9cb438275a8f3a29d26c343b867
peeraphol <ayopee001@gmail.com>
2026-03-13 14:26:39 +0700

    backbasic

--- src/app/layout-client.tsx
+++ src/app/layout-client.tsx
@@ -9,9 +9,6 @@ import Footer from "./components/Footer";
 import QueryString from "@/app/components/QueryString";
 import { AuthProvider } from "@/app/hooks/auth-hook";
 import ScrollTopButton from "@/app/components/ScrollTop";
-import SwipeBack from "@/app/components/SwipeBack";
-import usePreviousPathInStack from "@/app/hooks/usePreviousPathInStack";
-import useCanGoBack from "@/app/hooks/useCanGoBack";
 
 export default function RootLayoutClient({
   children,
@@ -20,43 +17,24 @@ export default function RootLayoutClient({
 }) {
   const [open, setOpen] = useState(false);
   const pathname = usePathname();
-  const previousPath = usePreviousPathInStack(pathname);
-  const canGoBack = useCanGoBack();
 
   useEffect(() => {
     window.czpSdk?.setBackButtonVisible?.(true);
   }, [pathname]);
 
-  useEffect(() => {
-    setOpen(false);
-  }, [pathname]);
-
   return (
     <AuthProvider>
+
       <Suspense fallback={null}>
         <QueryString />
       </Suspense>
 
-      {/* Navbar อยู่นอก SwipeBack */}
-      <Navbar onOpenMenu={() => setOpen((v) => !v)} />
+      <Navbar onOpenMenu={() => setOpen((i) => !i)} />
       <DrawerMenu open={open} onClose={() => setOpen(false)} />
 
-      <SwipeBack
-        enabled={!open && canGoBack}
-        fallbackHref="/"
-        mobileOnly
-        mobileMaxWidth={1024}
-        edge={20}
-        threshold={92}
-        velocityThreshold={620}
-        previousHref={previousPath}
-      >
-        <main className="min-h-screen bg-white pt-16">
-          {children}
-          <Footer />
-        </main>
-      </SwipeBack>
+      {children}
 
+      <Footer />
       <ScrollTopButton />
     </AuthProvider>
   );
